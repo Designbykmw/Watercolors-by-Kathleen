5e8ca6a4dcca16978f1249ee54be62fac3a1381a
Designbykmw <80842010+Designbykmw@users.noreply.github.com>
2021-06-29 13:33:11 -0400

    Add files via upload

---
+++ js/kwpaint.js
@@ -0,0 +1,87 @@
+//navbar//
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+ 
+/* Toggle mobile menu */
+function toggleMenu() {
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+         
+        // adds the menu (hamburger) icon
+        toggle.querySelector("a").innerHTML = "<i class=’fas fa-bars’></i>";
+    } else {
+        menu.classList.add("active");
+         
+        // adds the close (x) icon
+        toggle.querySelector("a").innerHTML = "<i class=’fas fa-times’></i>";
+    }
+}
+ 
+/* Event Listener */
+toggle.addEventListener("click", toggleMenu, false);
+
+
+const items = document.querySelectorAll(".item");
+ 
+/* Activate Submenu */
+function toggleItem() {
+  if (this.classList.contains("submenu-active")) {
+    this.classList.remove("submenu-active");
+  } else if (menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    this.classList.add("submenu-active");
+  } else {
+    this.classList.add("submenu-active");
+  }
+}
+ 
+/* Event Listeners */
+for (let item of items) {
+    if (item.querySelector(".submenu")) {
+      item.addEventListener("click", toggleItem, false);
+      item.addEventListener("keypress", toggleItem, false);
+    }   
+}
+/* Close Submenu From Anywhere */
+function closeSubmenu(e) {
+  let isClickInside = menu.contains(e.target);
+ 
+  if (!isClickInside && menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+  }
+}
+ 
+/* Event listener */
+document.addEventListener("click", closeSubmenu, false);
+
+//carousel//
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
